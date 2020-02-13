c0234c627595e0c13a74534b055fe07ae77f9b5f
Jorge G <JorgeGallardoG4@GMAIL.COM>
2020-02-13 17:48:46 -0600

    Proveedores

--- database/migrations/2020_02_12_171827_create_proveedores_table.php
+++ database/migrations/2020_02_12_171827_create_proveedores_table.php
@@ -13,9 +13,49 @@ class CreateProveedoresTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('proveedores');
+        Schema::dropIfExists('contactos');
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Nombre');
+            $table->string('Celular');
+            $table->string('Email');
+        });
         Schema::create('proveedores', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('Nombre');
+            $table->string('Clave');
+            $table->string('Calle');
+            $table->string('NExt')->nullable();
+            $table->string('Nint')->nullable();
+            $table->string('ECalle')->nullable();
+            $table->string('ECalle2')->nullable();
+            $table->string('Colonia');
+            $table->string('CP')->default('0')->change();
+            $table->string('Pais')->default('Mexico')->change();
+            $table->string('Estado')->default('Durango')->change();
+            $table->string('Municipio')->default('Dgo')->change();
+            $table->string('Poblacion')->default('Victoria de Durango')->change();
+            $table->string('Nacionalidad')->nullable();
+            $table->string('Clasificacion');
+            $table->string('Giro');
+            $table->string('RFC');
+            $table->string('CURP')->nullable();
+            $table->string('Telefono');
+            $table->string('Fax')->nullable();
+            $table->string('email');
+            $table->string('Pagina')->nullable();
+            $table->integer('DiasCredito')->default(0)->change();
+            $table->double('Saldo')->default(0)->change();
+            $table->double('Limite')->default(0)->change();
+            $table->integer('Forma')->default(0)->change();
+            $table->string('Titular');
+            $table->string('Banco')->nullable();
+            $table->string('Sucursal')->nullable();
+            $table->string('Cuenta');
+            $table->string('Clabe');
+            $table->BigInteger('id_contacto')->unsigned()->nullable();
+            $table->foreign('id_contacto')->references('id')->on('contactos')->onDelete('set null');
             $table->timestamps();
         });
     }
--- resources/views/Proveedores/FProveedores.blade.php
+++ resources/views/Proveedores/FProveedores.blade.php
@@ -40,15 +40,201 @@
         method="POST">
         @csrf
         <div class="p-5">
+            <div class="form-row mb-4">
+
+                <div class="col-2">
+                    <input type="text" class="form-control" name="clave" value="{{ old('clave') }}" placeholder="Clave"
+                        required>
+                </div>
+
+                <div class="col">
+                    <input type="text" class="form-control" name="nombre" value="{{ old('nombre') }}" required
+                        placeholder="Nombre">
+                </div>
+
+            </div>
+            <hr>
+
             <p class="h4 mb-4 "> Datos generales</p>
 
             <div class="form-row mb-4">
                 <div class="col">
-                    <input type="text" id="NProveedor" name="nombre" class="form-control"
-                        placeholder="Nombre del proveedor" value="{{ old('nombre') }}" required>
+                    <input type="text" class="form-control" name="calle" value="{{ old('calle') }}" placeholder="Calle"
+                        required>
+                </div>
+            </div>
+
+
+
+            <div class="form-row mb-4">
+
+                <div class="col-2">
+                    <input type="text" class="form-control" name="next" value="{{ old('next') }}"
+                        placeholder="Núm. Ext">
+                </div>
+
+                <div class="col-2">
+                    <input type="text" class="form-control" name="nInt" value="{{ old('nint') }}"
+                        placeholder="Núm. Int">
+                </div>
+
+                <div class="col">
+                    <input type="text" class="form-control" name="ecalle" value="{{ old('ecalle') }}"
+                        placeholder="Entre calle">
                 </div>
 
             </div>
+
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="ecalle2" value="{{ old('ecalle2') }}"
+                        placeholder="Y calle" required>
+                </div>
+
+                <div class="col-3">
+                    <input type="text" class="form-control" name="colonia" value="{{ old('colonia') }}" required
+                        placeholder="Colonia">
+                </div>
+                <div class="col-3">
+                    <input type="text" class="form-control" name="CP" value="{{ old('CP') }}"
+                        placeholder="Código postal">
+                </div>
+            </div>
+
+
+            <div class="form-row mb-4">
+
+                <div class="col">
+                    <input type="text" class="form-control" name="pais" value="{{ old('pais') }}" placeholder="País"
+                        required>
+                </div>
+
+                <div class="col">
+                    <input type="text" class="form-control" name="estado" value="{{ old('estado') }}"
+                        placeholder="Estado">
+                </div>
+
+            </div>
+
+
+            <div class="form-row mb-4">
+
+                <div class="col-4">
+                    <input type="text" class="form-control" name="municipio" value="{{ old('municipio') }}"
+                        placeholder="Municipio" required>
+                </div>
+
+                <div class="col-4">
+                    <input type="text" class="form-control" name="poblacion" value="{{ old('poblacion') }}"
+                        placeholder="Población">
+                </div>
+
+                <div class="col-4">
+                    <input type="text" class="form-control" name="nacionalidad" value="{{ old('nacionalidad') }}"
+                        placeholder="Nacionalidad">
+                </div>
+            </div>
+
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="clasificacion" value="{{ old('clasificacion') }}"
+                        placeholder="Clasificación" required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="giro" value="{{ old('giro') }}" placeholder="Giro">
+                </div>
+            </div>
+
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="rfc" value="{{ old('rfc') }}" placeholder="RFC"
+                        required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="curp" value="{{ old('curp') }}" placeholder="CURP">
+                </div>
+            </div>
+
+
+            <hr>
+            <p class="h4 mb-4 "> Saldo</p>
+            <!-- DiasCredito	Saldo	Limite	Forma	Titular	Banco	Sucursal	Cuenta	Clabe---->
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="forma" value="{{ old('forma') }}" placeholder="Forma"
+                        required>
+                </div>
+
+            </div>
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="dias" value="{{ old('dias') }}"
+                        placeholder="Días de crédito" required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="saldo" value="{{ old('saldo') }}" placeholder="Saldo">
+                </div>
+            </div>
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="limite" value="{{ old('limite') }}"
+                        placeholder="Límite de saldo" required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="titular" value="{{ old('titular') }}"
+                        placeholder="Titular">
+                </div>
+            </div>
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="banco" value="{{ old('banco') }}" placeholder="Banco"
+                        required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="sucursal" value="{{ old('sucursal') }}"
+                        placeholder="Sucursal">
+                </div>
+            </div>
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="cuenta" value="{{ old('cuenta') }}"
+                        placeholder="Cuenta" required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="clabe" value="{{ old('clabe') }}" placeholder="Clabe">
+                </div>
+            </div>
+
+            <hr>
+            <p class="h4 mb-4 "> Contacto</p>
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="text" class="form-control" name="telefono" value="{{ old('telefono') }}"
+                        placeholder="Teléfono" required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="fax" value="{{ old('fax') }}" placeholder="Fax">
+                </div>
+            </div>
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}"
+                        placeholder="E-mail" required>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" name="web" value="{{ old('web') }}"
+                        placeholder="Página web">
+                </div>
+            </div>
+
+
             <hr>
 
             <button class="btn btn-mdb-color py-3" style="text-transform: none; width:100%"> Crear nuevo
